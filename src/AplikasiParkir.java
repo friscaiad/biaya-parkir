73b495334588fc338bb0cdb7a2b24b95931d62d8
friscaiad <friscadewanti53@gmail.com>
2023-11-10 12:29:49 +0700

    biaya parkir

---
+++ src/AplikasiParkir.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Parkir {
+    private static final double BIAYA_5_JAM_PERTAMA = 1.0;
+    private static final double BIAYA_PER_JAM_SELANJUTNYA = 0.5;
+    private static final double BIAYA_FLAT_24_JAM = 15.0;
+    private static final int BATAS_WAKTU_FLAT_24_JAM = 24;
+
+    private int jamParkir;
+
+    public Parkir(int jamParkir) {
+        this.jamParkir = jamParkir;
+    }
+
+    public double hitungBiayaParkir() {
+        if (jamParkir <= 5) {
+            return BIAYA_5_JAM_PERTAMA;
+        } else if (jamParkir <= BATAS_WAKTU_FLAT_24_JAM) {
+            return BIAYA_5_JAM_PERTAMA + (jamParkir - 5) * BIAYA_PER_JAM_SELANJUTNYA;
+        } else {
+            return BIAYA_FLAT_24_JAM;
+        }
+    }
+}
+
+public class AplikasiParkir {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah jam parkir: ");
+        int jamParkir = scanner.nextInt();
+
+        // Membuat objek Parkir
+        Parkir parkir = new Parkir(jamParkir);
+
+        // Menghitung biaya parkir
+        double biayaParkir = parkir.hitungBiayaParkir();
+
+        // Menampilkan hasil
+        System.out.println("Biaya parkir yang harus dibayar: $" + biayaParkir);
+
+        // Menutup scanner
+        scanner.close();
+    }
+}
